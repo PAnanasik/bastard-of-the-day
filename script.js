5b1989837ead79a899f563d6558678eb4f421ba7
PAnanasik <57442428+PAnanasik@users.noreply.github.com>
2020-11-19 17:47:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let phrases = [
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  { text: 'Джейн Фриман', image: 'https://images-ext-2.discordapp.net/external/cd3Ws6y_ovji1R6iyFAsJOE_X3eNIDIvn0H3dYb-66c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/445215300535844892/a_101fe946798f5f0ab95b08225041f208.gif?width=230&height=230' },
+  ];
+  
+  function getRandomElement(arr) {
+    let randIndex = Math.floor(Math.random() * arr.length);
+    return arr[randIndex];
+  }
+  
+  let button = document.querySelector('.button');
+  let phrase = document.querySelector('.phrase');
+  let advice = document.querySelector('.advice');
+  let image = document.querySelector('.image');
+  
+  button.addEventListener('click', function () {
+    let randomElement = getRandomElement(phrases);
+    smoothly(phrase,"textContent", randomElement.text);
+    smoothly(image, "src",randomElement.image);
+  
+    if (randomElement.text.length > 40) {
+      advice.style.fontSize = '33px';
+    } else {
+      advice.style.fontSize = '42px';
+    }
+  });
+  
